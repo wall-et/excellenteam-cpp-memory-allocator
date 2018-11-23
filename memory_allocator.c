656e187b9948551cdb149b590fe2bcd1f434b72c
Efrat Traube <efratraube@gmail.com>
2018-11-23 02:57:31 -0800

    setting first block manager on init

--- memory_allocator.c
+++ memory_allocator.c
@@ -6,19 +6,27 @@
 
 struct MemoryAllocator {
     size_t m_memory_size;
-    void *m_memory_ptr;
-
+    void *m_memory_pool_ptr;
 };
 
 MemoryAllocator *MemoryAllocator_init(void *memoryPool, size_t size) {
     MemoryAllocator *mem = malloc(sizeof(MemoryAllocator));
-    mem->m_memory_ptr = memoryPool;
+    mem->m_memory_pool_ptr = memoryPool;
     if (size % sizeof(size_t)) {
         mem->m_memory_size = size - size % sizeof(size_t);
     }
+    *((size_t*)mem->m_memory_pool_ptr)=mem->m_memory_size;
+    *((size_t*)mem->m_memory_pool_ptr)|= 1 << 0;
     return mem;
 }
+/*
+void *MemoryAllocator_allocate(MemoryAllocator *allocator, size_t size) {
+    size_t* iter_ptr = allocator->m_memory_pool_ptr;
+    size_t i;
+    for(i = 0; i<*allocator->m_memory_size; ++i){
 
+    }
+}*/
 /*
 MemoryAllocator *MemoryAllocator_init(void *memoryPool, size_t size);
 
